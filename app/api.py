3786dd9be5878377511631b85587e5b4a710fa50
unknown <abu_h@live.co.uk>
2023-01-22 15:02:20 +0000

    reload

--- app/api.py
+++ app/api.py
@@ -31,4 +31,4 @@ async def combined_post(
     return JSONResponse(status_code=200, content={'prediction': prediction, 'probs': probas, 'classes': classes})
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host='0.0.0.0', port=8080, reload=True)
